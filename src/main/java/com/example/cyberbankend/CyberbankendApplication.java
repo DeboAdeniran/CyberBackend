de3b3225ad1cb5bb0d5ccbcf4ad865c7aceafa38
mope26 <ismaeelmopelola@gmail.com>
2025-03-17 16:39:54 +0100

    Teting

---
+++ src/main/java/com/example/cyberbankend/CyberbankendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyberbankend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberbankendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberbankendApplication.class, args);
+    }
+
+}
